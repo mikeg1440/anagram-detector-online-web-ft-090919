318adca92e4d47676a3ab5335fb27a4eff88fafb
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-17 15:08:08 -0400

    Working Anagram class to find anagam matches of a given word, all spec tests passing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require "pry"
+
+
+class Anagram
+
+  attr_accessor :word_to_match
+
+  def initialize(word)
+    @word_to_match = word
+  end
+
+  def match(words)
+
+    anagram_match = words.select do |word|
+      word.chars.sort == @word_to_match.chars.sort
+    end
+
+    anagram_match
+  end
+
+end
